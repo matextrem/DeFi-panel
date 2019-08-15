c33d5e84e677c6ab09c87e7aa087d0058ca0734a
Mati Dastugue <matias.dastugue@altoros.com>
2019-08-15 15:18:34 -0300

    update api uri from env

--- src/services/rateService.js
+++ src/services/rateService.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+const uri = process.env.REACT_APP_DEFI_API_URI;
 
 const RateService = {
     get: async () => {
-        const response = await axios.get('/rates/all');
+        const response = await axios.get(`${uri}/rates/all`);
         return response.data.protocols;
     }
 };
